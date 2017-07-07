956b981f53ad584f76af5c44ebd1cc9801aa6618
Agustin Cl <agustincl@gmail.com>
2017-07-07 21:07:34 +0200

    Random bicho

--- mypygame.py
+++ mypygame.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pygame
+import random
 
 # import inspect
 # from pprint import pprint
@@ -9,11 +10,19 @@ screenSize = (900, 700)
 screenSize = (620, 430)
 bichoSize = (30, 30)
 initialBackgroundPosition = (0, 0)
+gameSpeed = 50
+
+initialBichoPosition = (screenSize[0] / 2 - bichoSize[0]/2, screenSize[1] - bichoSize[1])
 
 initialTreasurePosition = (screenSize[0] / 2 - bichoSize[0]/2, bichoSize[1])
 
 projectFiles = 'assets/ProjectFiles/'
 screen = pygame.display.set_mode(screenSize)
+
+bichoInitialImage = projectFiles + 'player.png'
+treasureInitialImage = projectFiles + 'treasure.png'
+backgroundInitialImage = projectFiles + 'background.png'
+enemyInitialImage = projectFiles + 'enemy.png'
 
 # print(inspect.getmembers(pygame))
 
@@ -25,30 +34,31 @@ def main():
     pygame.init()
     finished = False
 
-    x = screenSize[0] / 2 - bichoSize[0]/2
-    y = screenSize[1] - bichoSize[1]
+    x = initialBichoPosition[0]
+    y = initialBichoPosition[1]
 
-    playerImage = pygame.image.load(projectFiles + 'player.png')
+    bichoPosition = (x, y)
+
+    playerImage = pygame.image.load(bichoInitialImage)
     playerImage = pygame.transform.scale(playerImage, bichoSize)
     playerImage = playerImage.convert_alpha()
 
-    treasureImage = pygame.image.load(projectFiles + 'treasure.png')
+    treasureImage = pygame.image.load(treasureInitialImage)
     treasureImage = pygame.transform.scale(treasureImage, bichoSize)
     treasureImage = treasureImage.convert_alpha()
-
     screen.blit(treasureImage, initialTreasurePosition)
 
-    backgroundImage = pygame.image.load(projectFiles + 'background.png')
+    backgroundImage = pygame.image.load(backgroundInitialImage)
     backgroundImage = pygame.transform.scale(backgroundImage, screenSize)
-
     screen.blit(backgroundImage, initialBackgroundPosition)
 
-
     # backgroundImage = backgroundImage.convert_alpha()
-
     frame = pygame.time.Clock()
 
-
+    # print pygame.font.get_fonts()
+    fontOne = pygame.font.SysFont("notosans", 18)
+    textWin = fontOne.render("Good Boy!!!", True, (0, 0, 0), (0, 0, 255))
+    textWinSize = (textWin.get_width(), textWin.get_height())
 
     while finished == False:
         for event in pygame.event.get():
@@ -61,7 +71,6 @@ def main():
             y -= 5
 
         # print pressedKeys
-
         # rectOne = pygame.Rect(x, y, 30, 30)  # x,y,w,h
 
         color = (0, 0, 255) # r,g,b
@@ -72,12 +81,42 @@ def main():
         screen.blit(treasureImage, initialTreasurePosition)
         screen.blit(playerImage, (x, y))
 
+        # screen.blit(textWin, (screenSize[0] / 2, screenSize[1] / 2))
         # pygame.draw.rect(screen, color, rectOne)
+
+        frame.tick(gameSpeed)
+
+        if collitionTreasure(x, y) == True :
+            bichoPosition = showWinText(textWin, screenSize, textWinSize)
+            (x, y) = changeBichoPos()
+
         pygame.display.flip()
 
-        frame.tick(y/10)
-
     return 0
+
+def collitionTreasure(x,y):
+    collitionState = False
+    if y >= initialTreasurePosition[1] and y <= initialTreasurePosition[1] + bichoSize[1]:
+        if x >= initialTreasurePosition[0] and x <= initialTreasurePosition[0] + bichoSize[0]:
+            collitionState = True
+        elif x + bichoSize[0] >= initialTreasurePosition[0] and x + bichoSize[0] <= initialTreasurePosition[0] + bichoSize[0]:
+            collitionState = True
+    elif y + bichoSize[1] >= initialTreasurePosition[1] and y + bichoSize[1] <= initialTreasurePosition[1] + bichoSize[1]:
+        if x >= initialTreasurePosition[0] and x <= initialTreasurePosition[0] + bichoSize[0]:
+            collitionState = True
+        elif x + bichoSize[0] >= initialTreasurePosition[0] and x + bichoSize[0] <= initialTreasurePosition[0] + bichoSize[0]:
+            collitionState = True
+    return collitionState
+
+def showWinText(textWin, screenSize, textWinSize):
+    screen.blit(textWin, (screenSize[0] / 2 - textWinSize[0] / 2, screenSize[1] / 2))
+
+def changeBichoPos(randPos = False):
+    (randX, randY) = initialBichoPosition
+    if randPos :
+        randX = random.randrange(0, screenSize[0])
+        randY = random.randrange(0, screenSize[1])
+    return randX, randY
 
 if __name__ == '__main__':
     main()
